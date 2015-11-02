05e70e36f417b984cdbe669c251d6b388df17f6e
humw <ssungle@gmail.com>
2015-11-02 20:56:38 +0800

    fix a bug

--- forests/server_urls.py
+++ forests/server_urls.py
@@ -4,5 +4,7 @@ from forests import views
 
 urlpatterns = [
     url(r'^$', views.server_list),
-    url(r'^(?P<server_id>[0-9]+)$', views.ServerDetail.as_view(), name='server-detail'),
+    url(r'^(?P<server_id>[0-9]+)$',
+        views.ServerDetail.as_view(),
+        name='server_detail'),
 ]
